5f4d5448204817ab2732b2a14f82d0ec7786a741
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-04-29 22:54:21 +0100

    📝 (docs): add JSDoc for Container component

--- src/app/shared/components/layout/container.tsx
+++ src/app/shared/components/layout/container.tsx
@@ -4,7 +4,14 @@ import { cn } from "@/app/shared/lib/utils"
 interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
 
 }
-
+/**
+ * The Container component ensures consistent layout formatting
+ * of the main content on each page, thus ensuring each page's main content has
+ * the same margins, padding etc...
+ *
+ * @param className TailwindCSS classes as children.
+ * @param ContainerProps interface for props passed into container as children.
+ * */
 export const Container = ({ className, ...props }: ContainerProps) => {
     return (
         <div
