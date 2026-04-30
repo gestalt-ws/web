ad8f9842aa2f065b8099129547f6572e9adabc25
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-04-30 12:08:24 +0100

    ✨ (api): add token getter method
    Retrieves jwt token from AuthStore.

--- src/app/api/rest-handler.ts
+++ src/app/api/rest-handler.ts
@@ -1,5 +1,6 @@
 import { useAuthStore } from "@/app/security/auth/authstore/auth-store.ts";
-
+//TODO: consider using interceptor pattern or some kind of functional factory here instead of class.
+// Futhermore, get userId won't be reactive so if user logs out it doesnt auto-re-render which is an issue.
 /**
  * Handles client-side HTTP requests and responses (POST, GET, PUT, CREATE, DELETE).
  * */
@@ -21,6 +22,16 @@ export class RestHandler {
     private get userId(): string | null {
         const { userId } = useAuthStore.getState();
         return userId;
+    }
+
+    /**
+     * Gets the JWT Token from keycloak kc.token in AuthStore.
+     *
+     * @return string | null of kc.token (as string or null val).
+     * */
+    private get token(): string | null {
+        const { token } = useAuthStore.getState();
+        return token;
     }
 
 }
